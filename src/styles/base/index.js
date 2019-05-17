739bd84ad286a132baa738d39c9d6e6819fb5890
Marco Bruno <marco.bruno.br@gmail.com>
2019-05-17 10:54:53 +0100

    Criado os components basicos como: color, reset, spacing e base

---
+++ src/styles/base/index.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+const Base = createGlobalStyle`
+    body {
+        background-color: var(--color-first);
+        font-family: 'Roboto Slab', serif;
+    }
+`;
+
+export default Base;
---
+++ src/styles/settings/spacing.js
@@ -0,0 +1,5 @@
+import { createGlobalStyle } from "styled-components";
+
+const Spacing = createGlobalStyle``;
+
+export default Spacing;
